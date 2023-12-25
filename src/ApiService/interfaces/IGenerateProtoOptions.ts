f7900ecc6a982fc6fa553c6f4cc339e7b9b9a7eb
Bogdan <n1ghtm6r9@gmail.com>
2023-12-25 23:51:59 +0200

    chore(): remove type

--- src/ApiService/interfaces/IGenerateProtoOptions.ts
+++
@@ -1,6 +0,0 @@
-import { IApiSchema } from './IApiSchema';
-
-export interface IGenerateProtoOptions {
-  serviceName: string;
-  schema: IApiSchema;
-}
--- src/ApiService/interfaces/index.ts
+++ src/ApiService/interfaces/index.ts
@@ -1,7 +1,6 @@
 export * from './IApiSchema';
 export * from './IApiService';
 export * from './IApiServiceFactory';
-export * from './IGenerateProtoOptions';
 export * from './ICreateApiServiceOptions';
 export * from './IApiServiceWithInfo';
 export * from './WebApiTypeEnum';
