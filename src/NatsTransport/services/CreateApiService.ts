4cdb8c6a25eff5878b9f15c42b8da0ef96527ad8
Bogdan <n1ghtm6r9@gmail.com>
2024-01-07 19:54:18 +0200

    fix(): bugs

--- src/NatsTransport/services/CreateApiService.ts
+++ src/NatsTransport/services/CreateApiService.ts
@@ -1,35 +1,45 @@
+import * as objHash from 'object-hash';
 import { ClientProxyFactory, Transport, NatsOptions } from '@nestjs/microservices';
 import { Inject, Injectable } from '@nestjs/common';
+import { ClientNats } from '@nestjs/microservices';
 import { configKey, IConfig } from '@nmxjs/config';
 import type { ICreateApiServiceOptions, IApiServiceWithInfo } from '../../ApiService';
 
 @Injectable()
 export class CreateApiService {
+  protected readonly clients = new Map<string, ClientNats>();
+
   constructor(@Inject(configKey) protected readonly config: IConfig) {}
 
   public async call(options: ICreateApiServiceOptions): Promise<IApiServiceWithInfo> {
     const natsOptions: NatsOptions = {
       transport: Transport.NATS,
       options: {
-        servers: this.config.transport.services.map(v => `nats://${v.host || '127.0.0.1'}:${v.port || 4222}`),
+        servers: this.config.transport.services.map(v => `nats://${v.host || '127.0.0.1'}:${v.port || 4222}`).sort(),
         ...(this.config.transport.keepaliveTimeMs ? { pingInterval: this.config.transport.keepaliveTimeMs } : {}),
       },
     };
 
-    const natsClient = ClientProxyFactory.create(natsOptions);
-    await natsClient.connect();
+    const key = objHash(natsOptions);
+    let client = this.clients.get(key);
+
+    if (!client) {
+      client = ClientProxyFactory.create(natsOptions) as ClientNats;
+      await client.connect();
+      this.clients.set(key, client);
+    }
 
     const service = Object.keys(options.schema).reduce((res, methodName) => {
-      res[methodName] = data => natsClient.send(`${options.subService || options.service}.${methodName}`, data);
+      res[methodName] = data => client.send(`${options.subService || options.service}.${methodName}`, data);
       return res;
     }, {});
 
     process.once('SIGTERM', () => {
-      natsClient.close();
+      client.close();
     });
 
     process.once('SIGINT', () => {
-      natsClient.close();
+      client.close();
     });
 
     return {
