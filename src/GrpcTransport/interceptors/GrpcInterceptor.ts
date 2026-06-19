d03e7092b35e39fe6c5b78f773ba27bcfac5f63d
Bogdan <n1ghtm6r9@gmail.com>
2026-06-19 16:57:30 +0300

    fix(): bugs

--- src/GrpcTransport/interceptors/GrpcInterceptor.ts
+++ src/GrpcTransport/interceptors/GrpcInterceptor.ts
@@ -1,7 +1,5 @@
 import { map, Observable } from 'rxjs';
 import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
-import { deepParseJson } from '../../ApiService/utils/deepParseJson';
-import { deepStringifyJson } from '../../ApiService/utils/deepStringifyJson';
 import { transformParseJson } from '../../ApiService/utils/transformParseJson';
 import { transformStringifyJson } from '../../ApiService/utils/transformStringifyJson';
 
@@ -10,15 +8,7 @@ export class GrpcInterceptor implements NestInterceptor {
   constructor(protected readonly key: string) {}
 
   public intercept(ctx: ExecutionContext, next: CallHandler): Observable<any> {
-    const requestData = ctx.switchToRpc().getData();
-    deepParseJson(requestData);
-    transformParseJson(`${this.key}.request`, requestData);
-    return next.handle().pipe(
-      map(res => {
-        transformStringifyJson(`${this.key}.response`, res);
-        deepStringifyJson(res);
-        return res;
-      }),
-    );
+    transformParseJson(`${this.key}.request`, ctx.switchToRpc().getData());
+    return next.handle().pipe(map(res => transformStringifyJson(`${this.key}.response`, res)));
   }
 }
